b6e0d468a3348be0a34bc76a5af925846249b276
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-09-20 11:36:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+def main():  
+
+    # listes holding values of x and y coordinates, in pixels 
+
+    pixels_x = []
+    pixels_y = [] 
+
+    l = []
+    i = 0
+
+
+    f = open("<.svg file path>", "r")
+
+
+    for line in f.readlines():
+
+        #print(line)
+
+
+        l = line.split()
+
+
+        if len(l) == 0:
+            continue 
+
+
+        if l[0] == "C":
+            i = 1
+            l = l[1:]
+
+
+        if i == 0:
+            continue
+
+
+        if l[-1] == "/>":
+            i = 0
+            l = l[:-1]
+            l[-1] = l[-1][:-1]
+
+
+        for j in range(0, len(l)):
+            x, y = l[j].split(",") 
+
+            pixels_x.append(x)
+            pixels_y.append(y)
+
+    f.close()
+
+    e = open("<.txt file path where pixels' x and y coordinates will be extracted>", "w")
+    e.write("# x, y in pixels")
+
+    for j in range(0, len(pixels_x)):
+        e.write("%s %s\n" % (pixels_x[j], pixels_y[j]))
+
+    e.close()
+
+
+if __name__ == '__main__':
+    main()
